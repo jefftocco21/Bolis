aaa559054824541595d646ea4dcc25fd2636399e
jnt369@gmail.com <jeff.tocco@lyonsexpress.com>
2022-04-18 11:04:45 -0400

    Adding basic navigation menu uptop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ['./*.html'], //look for any html in root
   theme: {
-    extend: {},
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+        gray: '#808080',
+        lightGray: '#D3D3D3'
+      }
+    },
   },
   plugins: [],
 }
