89d6c0522d210e850101896cb1066398f0bea3be
jnt369@gmail.com <jeff.tocco@lyonsexpress.com>
2022-04-22 10:10:35 -0400

    Adding some css and javascript menu for hamburger menu

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+btn.addEventListener('click', () => {
+    //toggle open class to turn menu icon into x and vice versa
+    btn.classList.toggle('open')
+
+    nav.classList.toggle('flex')
+    nav.classList.toggle('hidden')
+})
